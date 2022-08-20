c956b3e2952d266fa108ffa6b7932f721d6a369a
Izhan <57583461+1zhanN@users.noreply.github.com>
2022-08-20 19:19:41 +0500

    void default and main() syntax

---
+++ Mio/Test/JsonTesting/testing.java
@@ -0,0 +1,22 @@
+package JsonTesting;
+
+
+public class testing {
+    class a{
+    int s  = 0;
+}
+    
+    public static void main(String[] args){
+        
+        System.out.println("henlo");
+        System.out.println();
+        
+    }
+    class b{
+        
+    }
+    
+}
+class b{
+    
+}
