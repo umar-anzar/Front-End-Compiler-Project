9bf1189bcb0fd01b3050a5b13f4e326cf90a219e
Izhan <57583461+1zhanN@users.noreply.github.com>
2022-08-28 13:34:01 +0500

    break on space character ' '
    breaks word string on space character and appends in the word's list.

--- Mio/src/LexicalAnalyzer/Tokenizer.java
+++ Mio/src/LexicalAnalyzer/Tokenizer.java
@@ -8,6 +8,7 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;   // Import the Scanner class to read text files
 
 /**
@@ -21,19 +22,33 @@ public class Tokenizer {
         
         // Reader
         try{
+            
             File f = new File("src\\LexicalAnalyzer\\file.txt");    //Creation of File Descriptor for input file
             FileReader fr = new FileReader(f);                      //Creation of File Reader object
             BufferedReader br = new BufferedReader(fr);             //Creation of BufferedReader object
             
+            ArrayList<String> wordlist = new ArrayList<String>();
+            String temp = "";
             int c = 0;             
-            while((c = br.read()) != -1){               //Read char by Char       
-                char character = (char) c;              //converting integer to char
-                int ascii = character;                  //to get ascii of single character
-                System.out.println("'" +character+ "'" +" ascii: " + ascii);  //Display the Character
-                
+            
+            while((c = br.read()) != -1){       //reads Char by Char                         
+                if ((char)c == ' '){      
+                    wordlist.add(temp);
+                    temp = "";
+                }          
+                    temp += (char)c;
+                //int ascii = temp;               //getting ascii of single character
+                //System.out.println("'" +temp+ "'" +" ascii: " + ascii);  //Display the Character         
             }
+            
+            
+            if(temp != null){
+                wordlist.add(temp);
+            }
+            System.out.println(wordlist);
+
         }
-        
+
         catch(FileNotFoundException e){
             System.out.println("Error: File not found!");
         }       
