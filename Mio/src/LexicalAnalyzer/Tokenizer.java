5e40fbeb94a646ea98a66ebdd128e7bab6974311
Muhammad Umar Anzar <omer.anzar2@gmail.com>
2022-08-29 22:32:27 +0500

    Update Tokenizer.java

--- Mio/src/LexicalAnalyzer/Tokenizer.java
+++ Mio/src/LexicalAnalyzer/Tokenizer.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;   // Import the Scanner class to read text files
+
 
 /**
  *
@@ -17,18 +17,51 @@ import java.util.Scanner;   // Import the Scanner class to read text files
  */
 public class Tokenizer {
     public static void main(String[] args) {
+        
+        // Initialize var
+        String temp;
+        File f;
+        FileReader fr = null;
+        BufferedReader br = null;
+        
         //Initialize tokenList
+        
+        
         //ValidWords checker = new ValidWords();
         
-        // Reader
+        
+        // Initialize File Reader 
+        try {
+            //Creation of File Reader object
+            fr = new FileReader("src\\LexicalAnalyzer\\file.txt");
+            //Creation of BufferedReader object ("Raw file Reader")
+            br = new BufferedReader(fr);             
+        } catch (FileNotFoundException ex) {
+            System.out.println("File Not Found");
+            System.exit(0);
+        }
+        
+        
+        
+        // Reading File Char by Char and making Tokens
+        try {
+            br.read();
+            
+            
+            
+            
+            
+            br.close();
+            fr.close();
+        } catch (IOException ex) {
+            System.out.println("IO exception");
+        }
+        
+        
         try{
             
-            File f = new File("src\\LexicalAnalyzer\\file.txt");    //Creation of File Descriptor for input file
-            FileReader fr = new FileReader(f);                      //Creation of File Reader object
-            BufferedReader br = new BufferedReader(fr);             //Creation of BufferedReader object
-            
             ArrayList<String> wordlist = new ArrayList<String>();
-            String temp = "";
+            temp = "";
             int c = 0;             
             
             while((c = br.read()) != -1){       //reads Char by Char                         
@@ -48,16 +81,14 @@ public class Tokenizer {
             System.out.println(wordlist);
 
         }
-
-        catch(FileNotFoundException e){
-            System.out.println("Error: File not found!");
-        }       
+      
         catch (IOException e){
             System.out.println("Error: IOException");
         
         }
-
+        
         // Save Token List In txt
+        
             
     }
  
