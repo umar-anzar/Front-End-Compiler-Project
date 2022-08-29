dccc54f00324b80634cbb0efc72f6153dcb0f8f3
Izhan <57583461+1zhanN@users.noreply.github.com>
2022-08-29 23:40:42 +0500

    testing - regex syntax

--- Mio/Test/JsonTesting/testing.java
+++ Mio/Test/JsonTesting/testing.java
@@ -1,22 +1,39 @@
 package JsonTesting;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class testing {
-    class a{
-    int s  = 0;
-}
+
     
     public static void main(String[] args){
         
-        System.out.println((int)' ');
-        System.out.println("aasd".charAt(0));
+        while(true){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a test case: ");        
+        String test = scan.nextLine();
         
-    }
-    class b{
+        // regex
+        if (language("^([a-zA-Z_$][a-zA-Z\\d_$]*)$", test)){  //"[A-Za-z][A-Za-z0-9_]*"
+               System.out.println("Identifier constant");
+        }
+        else{
+            System.out.println("Not identifier constant");
+        }
         
-    }
-    
+        }
+
+
 }
-class b{
-    
+        public static boolean language(String RE, String test){
+        Pattern p = Pattern.compile(RE);
+        Matcher m = p.matcher(test);
+        return m.matches();
+     
+    }  
+
 }
+
+
