104f93838edc55e6cb870fa572d627dcd13e8df7
Muhammad Umar Anzar <omer.anzar2@gmail.com>
2022-09-29 23:15:45 +0500

    Update GeneralParser.py

--- New Language/Syntax Analyze Phase/CFG/GeneralParser.py
+++ New Language/Syntax Analyze Phase/CFG/GeneralParser.py
@@ -1,5 +1,7 @@
 class CFG:
+
     def __init__(self,start,cfg) -> None:
+
         self.index = 0
         self.word = []
         self.start = start
@@ -8,42 +10,58 @@ class CFG:
         self.theEnd = False
 
     def reset(self):
+
         self.index = 0
         self.word = []
         self.isOr = True
         self.theEnd = False
 
     def validate(self,word):
+
         self.word = word
         result = False
+
         if self.parser("<X>"):
+
             if len(word)-1 == self.index:
                 result = True
+
         self.reset() #Reset parameters to parse more
         return result,word[:-1]
 
     def parser(self,start):
+
         accept = False
         rule = self.cfg[start]
+
         for OR in rule:
+
             for k,terminal in enumerate(OR):
-                #print(rule)
+
                 accept = False
                 if terminal[0] == '<':#if non terminal
+
                     accept = self.parser(terminal)
-                    if not(accept):
+                    if not(accept): # break to find other rule of same non terminal
                         if k>0: 
                             self.theEnd = True
                             #We have enter in a production rule and there is no BACKTRACK then no BACK
                             #Because it failed to parse the other terminal
                         break
+
                 elif terminal == self.word[self.index]:
+
                     self.index += 1
                     accept = True
+
                 else:
-                    if terminal == 'null':
-                        accept = True
+
+                    if terminal == 'null': #return True because it is end condition
                         return True
+
+                    if k>0: #First set is true then second word false so no backtrack Faild to parse
+                        self.theEnd = True
+
                     break
             
             if self.theEnd:
@@ -81,6 +99,17 @@ x= "{ dt id { dt id { dt id { dt id { dt id *= floatConst } } }  } } ;"
 
 print(A.validate(x.split()+["$"]))
 
-x= "{ dt id += { dt id = intConst } ;"
+x= "{ dt id  { dt id = intConst } } ;"
 
+print(A.validate(x.split()+["$"]))
+
+
+
+A = CFG("<X>",
+        {
+            "<X>" : [ ["a","b"], ['c','d'] ]
+        }
+    )
+
+x= "c d"
 print(A.validate(x.split()+["$"]))
