3801913c23fa31a63ea35ca0d2869b4d3bd387bd
Muhammad Umar Anzar <omer.anzar2@gmail.com>
2022-10-31 21:28:28 +0500

    Semantic Phase Start

---
+++ Mio/src/SemanticAnalyzer/ClassDataTable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SemanticAnalyzer;
+
+/**
+ *
+ * @author omera
+ */
+public class ClassDataTable {
+    public String
+            name,
+            type,
+            Abstract,
+            accessModifier,
+            staticType,
+            constantType;
+}
---
+++ Mio/src/SemanticAnalyzer/MainTable.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SemanticAnalyzer;
+
+/**
+ *
+ * @author omera
+ */
+public class MainTable {
+    public String 
+            name,
+            type,
+            Abstract,
+            typeModifier,
+            size,
+            constantType,
+            extend,
+            accessModifier,
+            ref;     
+}
